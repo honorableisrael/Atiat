e3629e0f8d50060aa70399d86da1ad6f3a488ac0
Hilary <israel.hilary7@yahoo.com>
2020-01-29 10:37:40 +0100

    ourFleetOfCarsSection

--- autocomplete.js
+++ autocomplete.js
@@ -5,28 +5,31 @@
     var currentFocus;
     /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
+        var a, b,b1, i, val = this.value;
         /*close any already open lists of autocompleted values*/
+        b1 = document.createElement("DIV");
+        b1.innerHTML = `<div class="wrapper1"><p><b>Current Locations</b></p><p><i class="fa fa-map-marker"></i>  Lagos State</p><p><i class="fa fa-map-marker"></i>  Abuja </p><p><i class="fa fa-map-marker"></i>  Kwara State</p><p><i class="fa fa-map-marker"></i>  Imo State</p><p><i class="fa fa-map-marker"></i>  Ogun State</p></div>`
         closeAllLists();
         if (!val) { return false;}
         currentFocus = -1;
         /*create a DIV element that will contain the items (values):*/
         a = document.createElement("DIV");
         a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
+        a.setAttribute("class", "autocomplete-items ");
+        
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
           /*check if the item starts with the same letters as the text field value:*/
-          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
+        
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() ){
             b = document.createElement("DIV");
             /*make the matching letters bold:*/
             b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
             b.innerHTML += arr[i].substr(val.length);
             /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>"
             /*execute a function when someone clicks on the item value (DIV element):*/
             b.addEventListener("click", function(e) {
                 /*insert the value for the autocomplete text field:*/
@@ -36,7 +39,11 @@
                 closeAllLists();
             });
             a.appendChild(b);
+            a.appendChild(b1);
           }
+          if(arr[i].substr(0, val.length).toUpperCase() !== val.toUpperCase()){  
+            a.appendChild(b1);
+            }
         }
     });
     /*execute a function presses a key on the keyboard:*/
